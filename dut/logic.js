0a7e098bb7e9d51c4bffc43878b556d4d685c320
tushar8133 <tushar8133@gmail.com>
2019-08-20 23:12:19 +0530

    Changed comparison method from Object to Array

--- dut/logic.js
+++ dut/logic.js
@@ -10,15 +10,16 @@ var scriptEl = document.createElement('script');
 scriptEl.textContent = `
     
     var columnHeadings = [];
-    var htmlPageDataHolder = {};
-    var excelFileDataHolder = {};
+    var htmlPageDataHolder = [];
+    var excelFileDataHolder = [];
+    var bossArray = [];
 
 	var vm = angular.element(document.getElementById('faqcontent')).scope();
     var doctype1 = vm.kycFAQPageVm.idDocTypes;
     var doctype2 = vm.kycFAQPageVm.nonIdDocumentsObj;
     
-    doctype1.forEach(parent => parent.subType.forEach(child => htmlPageDataHolder[child.key] = [child.value, parent.key, parent.value, child.desc, child.inst]));
-    doctype2.forEach(parent => parent.subType.forEach(child => htmlPageDataHolder[child.key] = [child.value, parent.key, parent.value, child.desc, child.inst]));
+    doctype1.forEach(parent => parent.subType.forEach(child => htmlPageDataHolder.push([parent.key, child.key, parent.value, child.value, child.desc, child.inst])));
+    doctype2.forEach(parent => parent.subType.forEach(child => htmlPageDataHolder.push([parent.key, child.key, parent.value, child.value, child.desc, child.inst])));
 
     function createFileReader(){
         var fileChooser = document.createElement('input');
@@ -49,18 +50,17 @@ scriptEl.textContent = `
         var workbook = XLSX.read(data, {type:'binary'});
         workbook.SheetNames.forEach(function(sheetName) {
             var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-            var newObj = {};
+            excelFileDataHolder = [];
             var final = XL_row_object.map(curr => {
                 var arr = Object.values(curr).map(val => val.trim());
-                newObj[arr[0]] = [arr[1],arr[2],arr[3],arr[4],arr[5]];
+                excelFileDataHolder.push([arr[0],arr[1],arr[2],arr[3],arr[4],arr[5]]);
             })
-
-            excelFileDataHolder = newObj;
-            compare();
+            shallowCompare();
         })
     }
 
-    function compare(){
+    function shallowCompare(){
+    	bossArray = [];
         console.clear();
         console.log('%cBelow is website data', 'background: blue; color: white; display: inline;');
         console.table(htmlPageDataHolder);
@@ -69,52 +69,50 @@ scriptEl.textContent = `
 
         console.log('%cComparing both the data now!', 'background: blue; color: white; display: inline;');
 
-        if(Object.keys(htmlPageDataHolder).length !== Object.keys(excelFileDataHolder).length){
+        if(htmlPageDataHolder.length !== excelFileDataHolder.length){
             console.log('%cSubType Ids of this webpage and the uploaded CCD does not match, please check CCD and CQ lists.', 'background: red; color: black; display: inline;');
-            var set1 = new Set(Object.keys(htmlPageDataHolder));
-            var set2 = new Set(Object.keys(excelFileDataHolder));
-            var setsDifference = new Set([...set1].filter(x=>!set2.has(x)));
-            console.table(setsDifference);
             return;
         }
 
-        var bossArray = [];
-        Object.keys(htmlPageDataHolder).forEach(function(curr) {
-            var arr1 = htmlPageDataHolder[curr];
-            var arr2 = excelFileDataHolder[curr];
-            var discrepancy = false;
-            var newArray = [];
-            for (var i = 0; i < 6; i++) {
-                if (arr1[i] === arr2[i]) {
-                    newArray.push(null);
-                } else {
-                    newArray.push(arr1[i]);
-                    discrepancy = true;
+        for (var len = excelFileDataHolder.length, i = 0; i < len; i++) {
+            var excelSubTypeKey = excelFileDataHolder[i][1];
+            for (var j = 0; j < len; j++) {
+                var htmlSubTypeKey = htmlPageDataHolder[j][1];
+                if (htmlSubTypeKey == excelSubTypeKey) {
+                    deepCompare(htmlPageDataHolder[j], excelFileDataHolder[i]);
                 }
             }
-            if(discrepancy){
-                newArray.unshift(curr);
-                var tableObj = {
-                    "SubID Keys" : newArray[0],
-                    "SubID Value" : newArray[1],
-                    "ID Keys" : newArray[2],
-                    "ID Values" : newArray[3],
-                    "Descriptions" : newArray[4],
-                    "Instructions" : newArray[5]
-                }
-                bossArray.push(tableObj);
-            }
-        })
+        }
 
         if(bossArray.length > 0){
             console.log('%cDiscrepancy found!', 'background: red; color: black; display: inline;');
-            console.log('%cOnly differencial data will be shown in the below table. Similar results will be shown as "null". Also please ignore Column "0" of the table. This is to refer the SubType Ids', 'background: orange; color: black; display: inline;');
+            console.log('%cOnly differencial data will be shown in the below table. Similar results will be shown as "null". Also Column "0" and "1" of the table are just for reference.', 'background: orange; color: black; display: inline;');
             console.table(bossArray);
         }else{
             console.log('%cAll seems fine!', 'background: green; color: white; display: inline;');
         }
+    }
 
-
+    function deepCompare(htmlData, excelData) {
+        for (var i = 0; i < 6; i++) {
+            var discrepancy = false;
+            var newArray = [];
+            for (var i = 0; i < 6; i++) {
+                if (i <= 1) {
+                    newArray.push(htmlData[i]);
+                    continue;
+                }
+                if (htmlData[i] == excelData[i]) {
+                    newArray.push(null);
+                } else {
+                    newArray.push(htmlData[i]);
+                    discrepancy = true;
+                }
+            }
+            if (discrepancy) {
+                bossArray.push(newArray);
+            }
+        }
     }
 
     createFileReader();
